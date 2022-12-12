2757e787ed58fccda585ecd8b98d1c46429f0691
Mikhail_MSI <m89531276499@gmail.com>
2022-12-12 19:54:03 +0300

    create barrel(n)

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from random import sample
+from random import sample, randint
+import numpy as np
 
 
-def barrel():
+def barrel(n):
     '''
-    бочонок => для генерации случайно
-    взятого бачонка из мешка
-    :return: номер бочонка в заданном диапозоне
+    бочонки (1 - 90)
+    :return: n бочонков
     '''
 
     bar = [(i+1) for i in range(90)]
-    bag = sample(bar, 1)
+    bag = sample(bar, n)
     return  bag
 
+def card():
+    print('-' * 27)
+    for i in range(3):
+        if bool(randint(0, 1)):
+            print(' * '*9)
+    print('-' * 27)
+
+# card()
+# print(bool(randint(0,1)))
+
+# index_ = [i for i in range(9)]
+# indx_ = sample(index_, 5)
+# print(indx_)
+#
+# pattern = [1 if i in indx_ else ' ' for i in range(9)]
+# print(pattern)
+
+ex = barrel(15)
+print(ex)
